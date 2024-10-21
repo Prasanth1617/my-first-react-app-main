0a8f5cd8cf6a5ee739342927fb1a0f1d41284bc0
prasanthtom <prasanthpsh72@gmail.com>
2024-10-21 17:19:43 +0530

    task

---
+++ src/components/MainSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const MainSection = () => {
+  return (
+    <main className='' style={{margin:'auto',height:'auto'}}>
+       <div className='row container' style={{marginTop:'3rem',margin:'center'}}>
+        <div className='  col-3'>
+            <img className='image' src="1473079147382.jpg" alt="image" width={'250px'} />
+        </div>
+        <div className='content d-flex flex-column justify-content-start col-2'>
+            <h2>Prasanth U</h2>
+            <h4>___________________________</h4>
+            <h5>B.Sc Computer Science</h5>
+            <p>Contact:+91 1234567890</p>
+            <p>Email: prasanthpsh@gmail.com</p>
+        </div>
+        </div> 
+        
+    </main>
+    
+  )
+}
+
+export default MainSection
